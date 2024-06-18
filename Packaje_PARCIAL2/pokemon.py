4debe6a10f62b71bb803c2411e329813d11276c4
Cristian-Pentito <cristian_pentito@hotmail.com>
2024-06-18 12:31:03 -0300

    primeros pasos

--- Packaje_PARCIAL2/pokemon.py
+++ Packaje_PARCIAL2/pokemon.py
@@ -1,3 +1,4 @@
+from funciones import manejar_evento, dibujar
 import pygame
 import sys
 
@@ -13,13 +14,14 @@ color_backgraund = (AZUL_CLARO)
 
 posicion_titulo_x = 50
 posicion_titulo_y = 40
-ancho_ventana = 1500
+
+ancho_ventana = 1200
 alto_ventana = 1000
 
-ancho_ventana_1 =  600 
-alto_ventana_1 = 500
-posicion_ventana_1_x =  50
-posicion_ventana_1_y =  150
+ancho_imagen =  600 
+alto_imagen = 500
+posicion_imagen_x =  ancho_ventana//2
+posicion_imagen_y =  450
 
 
 
@@ -29,11 +31,13 @@ ventana = pygame.display.set_mode((ancho_ventana, alto_ventana))
 pygame.display.set_caption("¿Quien es ese Pokemon?")
 
 icono = pygame.image.load("Packaje_PARCIAL2\pikachu.png")
-imagen = pygame.image.load(r"Packaje_PARCIAL2\Imagenes_pokemones\IMG\1gen\charmeleon_black.png")
 pygame.display.set_icon(icono)
-img_rect = imagen.get_rect(center=(ancho_ventana//2, posicion_titulo_y))
+
+imagen = pygame.image.load(r"Packaje_PARCIAL2\Imagenes_pokemones\IMG\1gen\charmeleon_black.png")
+img_rect = imagen.get_rect(center=(posicion_imagen_x, posicion_imagen_y))
 
 fuente = pygame.font.SysFont("consolas", 40)
+
 texto = fuente.render("¿Quien es ese pokemon?", True, BLANCO)
 text_rect = texto.get_rect(center=(ancho_ventana//2, posicion_titulo_y))
 
@@ -48,17 +52,17 @@ while flag == True:
     lista_eventos = pygame.event.get()      
     ventana.fill(color_backgraund)
 
+
     for evento in lista_eventos:
         if evento.type == pygame.QUIT:      
             flag = False
 
     ventana.blit(texto, text_rect)
-    ventana.blit(imagen, (img_rect, posicion_ventana_1_y))
+    ventana.blit(imagen, img_rect)
 
-    
     pygame.display.update()
 
-nombre_ingresado = input("Cual es el pokemon")
+#nombre_ingresado = input("Cual es el pokemon")
 
 
 
